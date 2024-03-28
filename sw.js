d4e5fee016b022056547c3e2709d98740097869d
Do Duc Quan <dodquan@gmail.com>
2024-03-28 14:44:46 +0700

    Add service worker

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', function (event) {
+    // Perform install steps
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
