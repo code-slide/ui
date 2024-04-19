3382f83f6ef3ded3a106812a16d701f450bf336a
Do Duc Quan <99700700+ducquando@users.noreply.github.com>
2024-04-19 13:27:54 +0700

    Add codecov bundle

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,5 +20,10 @@ export default defineConfig({
         ...
         (process.env.NODE_ENV === 'production' ? [VitePWA({ injectRegister: null })] : []),
         react({}),
+        codecovVitePlugin({
+            enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+            bundleName: "<bundle project name>",
+            uploadToken: process.env.CODECOV_TOKEN,
+          }),
     ],
 });
